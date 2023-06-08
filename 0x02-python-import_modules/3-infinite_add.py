dd1bee1a509a3550521c0b8bebfee0c9f3078cc6
onohije <igbadumeonoh@.com>
2023-06-08 10:27:51 -0700

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -6,14 +6,6 @@ if __name__ == "__main__":
 
     result = 0
     for a in range(len(sys.argv) - 1):
-        result = sum(int(arg))
+        result += int(sys.argv[a + 1])
         result += int(sys.argv[a + 1])
     print("{}".format(result))
-
-"""import sys
-
-if __name__ == "__main__":
-    argv = sys.argv[1:]  # Exclude the script name from the arguments
-
-    result = sum(int(arg) for arg in argv)
-    print(result)"""
