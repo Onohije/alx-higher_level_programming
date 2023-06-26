d5742a0c2a7399306546b0e3fbadea4f437b78ed
onohije <igbadumeonoh@gmail.com>
2023-06-26 12:19:12 -0700

    raise exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def raise_exception():
+
+    """
+    Raise a TypeError exception.
+
+    Raises:
+        TypeError: This exception is raised intentionally.
+    """
+
+    raise TypeError
