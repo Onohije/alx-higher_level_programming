6a051e2790876b37a7df6205277379d5e95ca475
onohije <igbadumeonoh@gmail.com>
2023-06-27 04:42:59 -0700

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+    This class represents a square.
+
+    It provides methods and attributes for working with square shapes.
+    """
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
