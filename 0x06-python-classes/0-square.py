10c627f093e9700f36a4c83fa276ed95e9254bef
onohije <igbadumeonoh@gmail.com>
2023-06-27 04:24:57 -0700

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
+"""Define a class Square."""
+
 
 class Square:
-    """
-    This class represents a square.
-
-    It provides methods and attributes related to squares.
-    """
+    """Represent a square."""
     pass
