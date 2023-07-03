eb4b9a999cb9bfde2e1489620b3197853969f009
onohije <igbadumeonoh@gmail.com>
2023-07-03 10:03:34 -0700

    lazy matrix

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
-lazy_matrix_mul = __import__('101-lazy_matrix_mul').lazy_matrix_mul
+# 101-lazy_matrix_mul.py
+"""Defines a matrix multiplication function using NumPy."""
+import numpy as np
 
-print lazy_matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]])
-print lazy_matrix_mul([[1, 2]], [[3, 4], [5, 6]])
+
+def lazy_matrix_mul(m_a, m_b):
+    """Function to multiply two matrices using NumPy.
+
+    Args:
+        m_a (list): The first matrix represented as a nested list.
+        m_b (list): The second matrix represented as a nested list.
+
+    Returns:
+        np.ndarray: The resulting matrix after the multiplication.
+
+    Raises:
+        ValueError: If the matrices cannot be multiplied.
+    """
+
+    return (np.matmul(m_a, m_b))
