db82f25bc5a3e9a610d8af45a08b29973d860ce4
Onohije <igbadumeonoh@gmail.com>
2023-09-12 06:22:33 -0700

    js warm up

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const argv = process.argv.slice(2);
 if (argv.length > 1) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (argv.length === 1) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
