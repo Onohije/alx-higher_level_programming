101aafbe55623f215a0a9a7fce9bf96abd1edb76
Onohije <igbadumeonoh@gmail.com>
2023-09-29 13:31:23 -0700

    python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,13 +5,16 @@ import urllib.request
 
 
 def main():
+    """
+    Funtion to print a response of a specific url
+    """
     url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as r:
-        res = r.read()
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(res)))
-        print('\t- content: {}'.format(res))
-        print('\t- utf8 content: {}'.format(res.decode('utf8')))
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode('utf8')))
 
 
 if __name__ == "__main__":
