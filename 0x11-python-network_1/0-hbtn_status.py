c34271354929bc913ee450604ad3d88b10763026
Onohije <igbadumeonoh@gmail.com>
2023-09-29 13:25:21 -0700

    python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """A Python script that fetches data from https://alx-intranet.hbtn.io/status
-   and displays information about the response. """
-
+   and displays information about the response."""
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as r:
-    res = r.read()
-    print('Body response:')
-    print('\t- type: {}'.format(type(res)))
-    print('\t- content: {}'.format(res))
-    print('\t- utf8 content: {}'.format(res.decode('utf-8')))
+
+def main():
+    url = 'https://intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as r:
+        res = r.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(res)))
+        print('\t- content: {}'.format(res))
+        print('\t- utf8 content: {}'.format(res.decode('utf8')))
+
+
+if __name__ == "__main__":
+    main()
