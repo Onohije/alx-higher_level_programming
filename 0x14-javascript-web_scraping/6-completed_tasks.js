0ef501c91d76f717920f6ba4967cb8daa61f84d3
Onohije <igbadumeonoh@gmail.com>
2023-10-24 15:29:26 -0700

    web scraping

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  process.exit();
+}
+
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  const result = {};
+
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const todos = JSON.parse(body);
+    for (const todo of todos) {
+      if (todo.completed) {
+        if (result[todo.userId] === undefined) {
+          result[todo.userId] = 0;
+        }
+        result[todo.userId] += 1;
+      }
+    }
+    console.log(result);
+  }
+});
